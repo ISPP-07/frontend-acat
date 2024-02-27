da534b2ce37b1d4e8c59ce50774460e5ab1c572d
Nicolás Herrera Lobo <nicoloboo02@gmail.com>
2024-02-27 18:44:10 +0100

    fix: Add change to modal #9
    A change has been added so that it is
    not possible to tab out of the modal

--- src/app/components/modal.jsx
+++ src/app/components/modal.jsx
@@ -1,6 +1,22 @@
 'use client'
+
+import { useEffect } from "react";
+
 export default function Modal ({isVisible, onClose}) {
     if(!isVisible) return null
+    useEffect(() => {
+        const disableTabNavigation = (x) => {
+          if (isVisible ) {
+            x.preventDefault();
+          }
+        };
+    
+        document.addEventListener('keydown', disableTabNavigation);
+    
+        return () => {
+          document.removeEventListener('keydown', disableTabNavigation);
+        };
+      }, [isVisible, onClose]);
 
     const toClose=(x)=>{
         if(x.target.id==="close") {
@@ -8,12 +24,13 @@ export default function Modal ({isVisible, onClose}) {
 
         }
     }
-    return (
+    return (  
         <div className= {`fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center ${isVisible ? 'z-30' : 'z-0'} `} id="close" onClick={toClose}>
         <div className="w-[600px] h-[200px] ">
             <button className="text-gray-500 text-xl" onClick={onClose}>X</button>
             <div className="bg-white p-2 rounded-3xl text-black font-Varela">Aquí iría el contenido
-            del modal, el cual se hacemás y más grande a medida que vas añadiendo cosas </div>
+            del modal, el cual se hacemás y más grande a medida que vas añadiendo cosas
+             </div>
         </div>
 
         </div>
