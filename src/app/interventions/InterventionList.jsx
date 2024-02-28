43d178b74739b45416335c20b980865f9d6bd8c0
Manuelgruiz <manuelgruiz22@gmail.com>
2024-02-29 00:59:54 +0100

    fix: Fixing grid columso #1

--- src/app/interventions/InterventionList.jsx
+++ src/app/interventions/InterventionList.jsx
@@ -2,12 +2,12 @@ import InterventionCard from './InterventionCard'
 
 const InterventionList = ({ interventions }) => {
 	return (
-		<div className="max-w-fit bg-white p-4 rounded-lg shadow">
-			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-7 overflow-y-scroll relative top-28">
-				{interventions.map(intervention => (
+		<div className="max-w-screen-2xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 overflow-y-scroll relative top-28">
+			{interventions.map(intervention => (
+				<div key={intervention.id} className="w-full">
 					<InterventionCard key={intervention.id} intervention={intervention} />
-				))}
-			</div>
+				</div>
+			))}
 		</div>
 	)
 }
