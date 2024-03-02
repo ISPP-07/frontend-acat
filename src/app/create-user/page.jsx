825700c8ab39a41af4bab0727f501af80b4f2ada
JMGarCas <jmgarciacasillas135@gmail.com>
2024-03-02 13:54:44 +0100

    fix: import React for Codacy

--- src/app/create-user/page.jsx
+++ src/app/create-user/page.jsx
@@ -1,6 +1,8 @@
 import CreateUserForm from '../components/CreateUserForm'
 import Sidebar from '../components/sidebar'
-
+/* eslint-disable no-unused-vars */
+import React from 'react'
+/* eslint-enable no-unused-vars */
 export default function Home() {
 	return (
 		<main className="flex bg-white wallpaper w-screen h-screen text-black">
