e8be35096d29ac16758d8a6da76dfd97980f7268
sebasruii <frabenrui1@alum.us.es>
2024-03-02 10:06:14 +0100

    fix: Merge test fixes #4

--- src/app/beneficiaries/page.jsx
+++ src/app/beneficiaries/page.jsx
@@ -1,14 +1,11 @@
-'use client'
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react'
+import React from 'react'
 /* eslint-enable no-unused-vars */
-import CreateModal from './create'
 import Link from 'next/link.js'
 import Card from './card.jsx'
 import { fetchDataBeneficiaries } from './fetch.js'
 
 export default async function BeneficiariesList() {
-	const [verModal, setVerModal] = useState(false)
 	const data = await fetchDataBeneficiaries()
 	return (
 		<div className="max-w-fit grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 overflow-y-scroll relative top-28">
@@ -17,14 +14,6 @@ export default async function BeneficiariesList() {
 					<Card key={beneficiary.id} beneficiary={beneficiary} />
 				</Link>
 			))}
-			<CreateModal isVisible={verModal} onClose={() => setVerModal(false)} />
-			<button
-				className={`bg-cyan-500 rounded-full ${verModal ? 'blur' : ''}`}
-				onClick={() => setVerModal(true)}
-			>
-				{' '}
-				Dar de alta
-			</button>
 		</div>
 	)
 }
