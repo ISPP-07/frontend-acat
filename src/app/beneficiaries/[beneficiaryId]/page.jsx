9f1b2cc6d2a40dccbb8506eaf071159e01638cbf
sebasruii <frabenrui1@alum.us.es>
2024-03-03 10:26:23 +0100

    refactor: Improve styles #13

--- src/app/beneficiaries/[beneficiaryId]/page.jsx
+++ src/app/beneficiaries/[beneficiaryId]/page.jsx
@@ -5,31 +5,56 @@ import { fetchDataBeneficiary } from './fetch'
 import User from '@/app/components/icons/user'
 import Phone from '@/app/components/icons/phone'
 import Location2 from '@/app/components/icons/location-2'
+import IconButton from './iconButton'
+import InboxArrowDown from '@/app/components/icons/inbox-arrow-down'
+import Pen3 from '@/app/components/icons/pen-3'
+import MockSwitch from './mockSwitch'
 
 export default async function BeneficiaryDetails({ params }) {
 	const beneficiary = await fetchDataBeneficiary(params.beneficiaryId)
 
 	return (
 		<div className="font-Varela text-black relative top-12 text-lg">
-			<div className="text-2xl flex">
-				<User height="34" width="34" />
-				<strong>{beneficiary.name}</strong>
+			<div className="text-2xl flex items-center justify-between">
+				<div className="flex justify-self-start">
+					<User height="34" width="34" />
+					<strong>{beneficiary.name}</strong>
+				</div>
+				<div className="flex justify-end  gap-2 mr-80">
+					<IconButton
+						icon={InboxArrowDown}
+						disabled={true}
+						color="bg-yellow-300 hover:bg-yellow-400 active:bg-yellow-500"
+					/>
+					<IconButton
+						icon={Pen3}
+						disabled={true}
+						color=" bg-blue-300 hover:bg-blue-400 active:bg-blue-500"
+					/>
+				</div>
 			</div>
-			<div className="flex">
+			<div className="flex justify-between mt-4">
 				<div>
-					<button className="bg-green-700  w-5/6 h-10 text-white relative rounded-full font-Varela text-sm right">
+					<button className="bg-green-700  p-2 h-10 text-white relative rounded-full font-Varela text-sm right">
 						+ Nueva intervención
 					</button>
 				</div>
-				<div className="left">FINALIZADO</div>
+				<div className="mr-80 flex">
+					<div className="left mr-4">FINALIZADO</div>
+					<MockSwitch />
+				</div>
 			</div>
 			<hr className="border-t border-dotted my-8" />
-			<div className="flex">
-				<Phone />
+			<div className="flex mr">
+				<div className="mr-2">
+					<Phone />
+				</div>
 				<strong>{beneficiary.contact_phone}</strong>
 			</div>
 			<div className="flex">
-				<Location2 />
+				<div className="mr-2">
+					<Location2 />
+				</div>
 				<strong>{beneficiary.address}</strong>
 			</div>
 			<hr className="border-t border-dotted my-8" />
@@ -72,7 +97,7 @@ export default async function BeneficiaryDetails({ params }) {
 						{beneficiary.dossier_number}
 					</p>
 				</div>
-				<div className="mt-6">
+				<div className="mt-6 w-96">
 					<p>
 						<strong style={{ color: '#4B7BECFF' }}>Observaciones:</strong>
 					</p>
