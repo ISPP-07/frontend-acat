d2c8e3a98f70dfcbdc2a84cec4fc59ef24adcd4f
Manuelgruiz <manuelgruiz22@gmail.com>
2024-03-11 18:35:40 +0100

    fix:Solve vercel problems  #38

--- src/app/beneficiaries/fetch.js
+++ src/app/beneficiaries/fetch.js
@@ -3,11 +3,8 @@ export function fetchDataBeneficiaries() {
 	const BASEURL = process.env.BASEURL
 	try{
 		const beneficiaries = axios.get(
-			`${BASEURL}/acat/patient`
-		)
-		return beneficiaries.then(response => {
-			return response.data
-		})
+			`${BASEURL}/acat/patient`)
+		return beneficiaries.data
 	}
     catch (error) {
 		return null
