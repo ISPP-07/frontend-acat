138a40f929d68f01a12eb2acd114e0b527196d75
Ronald752 <ronmonalb@alum.us.es>
2024-03-14 11:35:16 +0100

    feat: Add change mockapi

--- src/app/interventions/fetchIntervention.js
+++ src/app/interventions/fetchIntervention.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 export async function fetchDataInterventions() {
-
+	const BASEURL = process.env.NEXT_PUBLIC_BASE_URL
 	try{
 		const interventions = await axios.get(
-			'https://65df8753ff5e305f32a26916.mockapi.io/Interventions/interventions'
+			`${BASEURL}/acat/appointment/`
 		)
 		return interventions.data
 	}
