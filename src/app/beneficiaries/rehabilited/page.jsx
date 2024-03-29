9f893bda0c1952ece239062b20af172b1aeaca88
Manuelgruiz <manuelgruiz22@gmail.com>
2024-03-29 18:16:32 +0100

    fix:Delete import button  #52

--- src/app/beneficiaries/rehabilited/page.jsx
+++ src/app/beneficiaries/rehabilited/page.jsx
@@ -7,9 +7,6 @@ import CardBeneficiary from '../../components/cardBeneficiary.jsx'
 import Sidebar from '../../components/sidebar.jsx'
 import Searchbar from '../../components/searchbar.jsx'
 import { Rehabilited } from './fetch.js'
-import Image from 'next/image'
-import exportData from '../../exportData.js'
-import axios from 'axios'
 import CreateModal from '../create.jsx'
 
 export default function BeneficiariesList() {
@@ -18,23 +15,6 @@ export default function BeneficiariesList() {
 
 	const toggleModal = () => {
 		setShowModal(!showModal)
-	}
-
-	const handleFileChange = async event => {
-		const selectedFile = event.target.files[0]
-		try {
-			const formData = new FormData()
-			formData.append('file', selectedFile)
-			await axios.post('url/de/import', formData, {
-				headers: {
-					'Content-Type': 'multipart/form-data'
-				}
-			})
-			alert('Datos importados correctamente')
-		} catch (error) {
-			console.error(error)
-			alert('Error al importar los datos')
-		}
 	}
 
 	useEffect(() => {
@@ -59,42 +39,7 @@ export default function BeneficiariesList() {
 			</Suspense>
 			<div className="w-full h-full flex flex-col items-center">
 				<Searchbar handleClick={toggleModal} stext="Dar de alta" />
-				<div className="flex flex-row">
-					<button
-						className=" bg-green-400 h-8 w-8 rounded-full shadow-2xl mt-3 mr-2"
-						onClick={() =>
-							exportData(data, 'Beneficiados', [
-								'id',
-								'alias',
-								'birthday',
-								'isFinished'
-							])
-						}
-						data-testid="export-button"
-					>
-						<Image
-							alt="Exportar a excel"
-							src="/excel.svg"
-							className="ml-2"
-							width={15}
-							height={15}
-						></Image>
-					</button>
-					<label
-						htmlFor="file"
-						className="bg-green-400 w-32 h-6 mt-4 rounded-full font-Varela text-white cursor-pointer text-center text-sm"
-					>
-						Importar datos
-					</label>
-					<input
-						type="file"
-						id="file"
-						onChange={handleFileChange}
-						style={{ display: 'none' }}
-						accept=".xls"
-						data-testid="file"
-					/>
-				</div>
+
 				<div className="container p-10 flex flex-wrap gap-5 justify-center items-center">
 					<Suspense fallback={<div>Cargando...</div>}>
 						{data &&
