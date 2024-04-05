9e5e92e379eda64467769d5caba00e369d035237
AntonioRodriguezRuiz <antoniorodriguezruiz2002@gmail.com>
2024-04-05 17:13:48 +0200

    fix: Fallback value for patinent on intervention

--- src/app/components/InterventionDetailsView.jsx
+++ src/app/components/InterventionDetailsView.jsx
@@ -20,36 +20,40 @@ export default function InterventionDetailsView({ intervention }) {
 	}
 
 	return (
-		<section className="flex flex-col gap-3 w-full">
-			<article className="flex items-center w-full">
-				<p className="font-Varela w-fit text-blue-500 font-bold mr-2">
+		<section className='flex flex-col gap-3 w-full'>
+			<article className='flex items-center w-full'>
+				<p className='font-Varela w-fit text-blue-500 font-bold mr-2'>
 					Paciente:
 				</p>
-				<p className="p-1 w-full">{intervention.patient.alias}</p>
+				<p className='p-1 w-full'>
+					{intervention.patient.alias
+						? intervention.patient.alias
+						: intervention.patient.name}
+				</p>
 			</article>
-			<article className="flex items-center w-full">
-				<p className="font-Varela text-blue-500 font-bold mr-2">
+			<article className='flex items-center w-full'>
+				<p className='font-Varela text-blue-500 font-bold mr-2'>
 					Fecha de atención:
 				</p>
-				<p className="p-1">{formatDate(intervention.date)}</p>
+				<p className='p-1'>{formatDate(intervention.date)}</p>
 			</article>
-			<article className="flex items-center w-full">
-				<p className="font-Varela text-blue-500 font-bold mr-2">Tipología:</p>
-				<p className="p-1">{intervention.typology}</p>
+			<article className='flex items-center w-full'>
+				<p className='font-Varela text-blue-500 font-bold mr-2'>Tipología:</p>
+				<p className='p-1'>{intervention.typology}</p>
 			</article>
-			<article className="flex items-center w-full">
-				<p className="font-Varela text-blue-500 font-bold mr-2">Técnico:</p>
-				<p className="p-1">{intervention.technician}</p>
+			<article className='flex items-center w-full'>
+				<p className='font-Varela text-blue-500 font-bold mr-2'>Técnico:</p>
+				<p className='p-1'>{intervention.technician}</p>
 			</article>
-			<article className="flex items-center w-full">
-				<p className="font-Varela text-blue-500 font-bold mr-2">Motivo</p>
-				<p className="p-1">{intervention.reason}</p>
+			<article className='flex items-center w-full'>
+				<p className='font-Varela text-blue-500 font-bold mr-2'>Motivo</p>
+				<p className='p-1'>{intervention.reason}</p>
 			</article>
-			<article className="flex flex-col w-full">
-				<p className="font-Varela text-blue-500 font-bold mr-2">
+			<article className='flex flex-col w-full'>
+				<p className='font-Varela text-blue-500 font-bold mr-2'>
 					Observaciones:
 				</p>
-				<p className="p-1">{intervention.observations}</p>
+				<p className='p-1'>{intervention.observations}</p>
 			</article>
 		</section>
 	)
