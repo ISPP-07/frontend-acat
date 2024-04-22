a6bce3f4e8083c0cbb57e2e06d41c57c8160b4e0
AntonioRodriguezRuiz <antoniorodriguezruiz2002@gmail.com>
2024-04-22 19:23:18 +0200

    fix: CORS error on retrieving finalized beneficiaries

--- src/app/beneficiaries/rehabilited/fetch.js
+++ src/app/beneficiaries/rehabilited/fetch.js
@@ -4,7 +4,7 @@ export async function Rehabilited() {
 	const BASEURL = process.env.NEXT_PUBLIC_BASE_URL
 	try {
 		const beneficiaries = await axios
-			.get(`${BASEURL}/acat/patient/`)
+			.get(`${BASEURL}/acat/patient`)
 			.catch(err => {
 				if (err.response.status === 403 || err.response.status === 401) {
 					// redirect to login
